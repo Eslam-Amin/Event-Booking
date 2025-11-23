2abc337c169640005840a892c9dcfa35116cb227
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-23 17:15:10 +0200

    Created get events endpoint

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"net/http"
 
+	"example.com/event-booking/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/ping", func (context *gin.Context){		
+	server.GET("/ping", func (context *gin.Context){
+	
 		context.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+
+	server.GET("/events", func (context *gin.Context){
+		context.JSON(http.StatusOK, gin.H{
+			"data": models.GetAllEvents(),
 		})
 	})
 
