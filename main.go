4e49a1eaf8e1001cd5ac07b186e4e4f213456700
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-23 19:22:33 +0200

    Separated getEvents, and createEvent controller functions

--- main.go
+++ main.go
@@ -20,34 +20,45 @@ func main() {
 		})
 	})
 
-	server.POST("/events", func (context *gin.Context){
-		var event models.Event
-		err := context.ShouldBindJSON(&event)
-		fmt.Println("err: ",err)
-		if err != nil {	
-			context.JSON(http.StatusBadRequest, gin.H{
-				"message": err,
-			})
-		return
-		}
-
-		event.ID = 1
-		event.UserID = 1
-		event.Save()
-		context.JSON(http.StatusCreated, gin.H{
-			"message":"Event created successfully",
-			"data": event,
-		})
-
-
-
-	})
-
-	server.GET("/events", func (context *gin.Context){
-		context.JSON(http.StatusOK, gin.H{
-			"data": models.GetAllEvents(),
-		})
-	})
+	server.POST("/events", createEvent)
+	server.GET("/events", getAllEvents)
 
 	server.Run(":8080")
+}
+
+func createEvent(context *gin.Context){
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil{
+		fmt.Println(err)
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Couldn't parse request data event.",
+		})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Event created successfully",
+		"data": event,
+	})
+
+}
+
+
+func getAllEvents(context *gin.Context) {
+	events, err :=  models.GetAllEvents()
+	if err != nil{
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"data": events,
+	})
 }
