6f7997ae9fafc5c2e7bafd1bfd6b772a706a8316
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-24 00:50:27 +0200

    used register routes

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
 
 	"example.com/event-booking/db"
-	"example.com/event-booking/models"
+	"example.com/event-booking/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
-
+	routes.RegisterRoutes(server)
+	
 	server.GET("/ping", func (context *gin.Context){
 	
 		context.JSON(http.StatusOK, gin.H{
@@ -21,75 +20,5 @@ func main() {
 		})
 	})
 
-	server.POST("/events", createEvent)
-	server.GET("/events", getAllEvents)
-	server.GET("/events/:id", getEventById)
-
 	server.Run(":8080")
-}
-
-func createEvent(context *gin.Context){
-  var event models.Event
-
-    err := context.ShouldBindJSON(&event)
-    if err != nil {
-        context.JSON(http.StatusBadRequest, gin.H{
-            "message": "Couldn't parse request data.",
-        })
-        return
-    }
-
-    event.UserID = 1
-
-    err = event.Save()
-    if err != nil {
-			fmt.Println(err)
-			context.JSON(http.StatusInternalServerError, gin.H{
-            "message":"Couldn't create event, try again later!",
-        })
-        return
-    }
-
-    context.JSON(http.StatusOK, gin.H{
-        "message": "Event created successfully",
-        "data": event,
-    })
-}
-
-
-
-func getAllEvents(context *gin.Context) {
-	events, err :=  models.GetAllEvents()
-	if err != nil{
-		context.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Couldn't fetch events, try again later!",
-		})
-		return
-	}
-
-	context.JSON(http.StatusOK, gin.H{
-		"data": events,
-	})
-}
-
-func getEventById(context *gin.Context){
-	eventId, err := strconv.ParseInt(context.Param("id"), 10 , 64)
-	if err != nil{
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Couldn't parse request data.",
-		})
-		return
-	}
-	event, err := models.GetEventById(eventId)
-	if err != nil{
-		context.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Couldn't fetch event, try again later!",
-		})
-		return
-	}
-
-	context.JSON(http.StatusOK, gin.H{
-		"message":"Event fetched successfully",
-		"data":event,
-	})
 }
