7fb8fa4acb83b0a693ba116e00d7ed3e8168d0cb
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-24 02:33:50 +0200

    Created ComparePasswords in the hash util

--- utils/hash.go
+++ utils/hash.go
@@ -6,3 +6,8 @@ func HashPassword(password string) (string, error){
 	hashedPasswordBytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(hashedPasswordBytes), err
 }
+
+func ComparePasswords(password, hashedPassword string ) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
