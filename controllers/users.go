81e9513aba90038238eb68d9b2cebe8a7cc81e7c
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-24 17:44:52 +0200

    Created User Regisrtation controller function

--- controllers/users.go
+++ controllers/users.go
@@ -45,3 +45,28 @@ func GetUserById(context *gin.Context) {
 		"data":    user,
 	})
 }
+
+func GetUsersRegistration(context *gin.Context) {
+	userId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error":   err.Error(),
+			"message": "Invalid user ID",
+		})
+		return
+	}
+
+	registrations, err := models.GetRegistrationsByUserId(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"error":   err.Error(),
+			"message": "Couldn't fetch registrations, try again later!",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Registrations fetched successfully",
+		"data":    registrations,
+	})
+}
