d52c79414664b18108b350aec1bc0a13f4d088e0
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-24 14:33:35 +0200

    Created a validate token func

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,4 +17,35 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(secretKey))
+}
+
+
+func ValidateToken(token string) error {
+	parsedToken, err :=jwt.Parse(token, func(token *jwt.Token)(interface{}, error){
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, jwt.ErrTokenMalformed
+		}
+		return secretKey, nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if !parsedToken.Valid {
+		return errors.New("invalid token!")
+	}
+
+	_, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return jwt.ErrTokenInvalidClaims
+	}
+
+	return nil
+
+
+	// email := claims["email"].(string) 
+	// userId := claims["userId"].(int64)
+	
+	// return nil
 }
